3ce84af322103d80901f6bcfa641443c854b2805
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-11 16:19:43 +0900

    프로젝트 변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,23 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("androidx.*")
+      }
     }
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
-
 dependencyResolutionManagement {
-    val appcompat_version = "1.7.0"
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
-rootProject.name = "KotilinTestApp"
-
-include(
-    ":app")
+rootProject.name = "KotlinTestApp"
+include(":app")
