c7ff32881f3923a44df146ab115b85b29eec9999
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-14 00:10:34 +0900

    Test

--- app/src/main/java/com/example/kotlintestapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/kotlintestapp/ui/screens/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.example.kotlintestapp.ui.screens
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,8 +8,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +21,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
@@ -29,39 +33,71 @@ import androidx.navigation.NavController
 fun LoginScreen(nav: NavController){
 
   Column(
-    modifier = Modifier.fillMaxWidth().padding(10.dp),
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(10.dp),
       horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement =    Arrangement.Center
 
   ){
     var username by remember {mutableStateOf(TextFieldValue())}
     var password by remember{mutableStateOf(TextFieldValue())}
-    Text(text = "Login Screen")
+var isError by remember { mutableStateOf(false) }
+    Text(text = "BUBAUM", textAlign = TextAlign.Center,
+      style = TextStyle(
+        color = Color.Blue,
+        fontSize = 42.sp,
+        fontWeight = FontWeight.Bold,
+      ),
+      modifier = Modifier
+        .fillMaxWidth()
+
+
+    )
+    Spacer(modifier = Modifier.height(20.dp))
+    Text(text = "로그인", textAlign = TextAlign.Center,
+      style = TextStyle(
+        color = Color.Blue,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+
+      ),)
+    Spacer(modifier = Modifier.height(10.dp))
   Column(
     modifier = Modifier.fillMaxWidth(),
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center
   ){
-    TextField(
-      shape = RoundedCornerShape(0.dp),
+    OutlinedTextField(
+      colors = outlinedTextFieldColors(
+        focusedBorderColor = Color.Blue,
+        unfocusedBorderColor = Color.Blue,
+        errorBorderColor = Color.Red,
+      ),
+      shape = RoundedCornerShape(5.dp),
       modifier = Modifier.fillMaxWidth(),
       value = username, onValueChange = { username = it}, label = { Text("UserName") })
     Spacer(modifier = Modifier.height(10.dp))
-    TextField(
-      shape = RoundedCornerShape(0.dp),
-      modifier = Modifier.fillMaxWidth().border( width= 0.dp, color = Color.Black, shape = RoundedCornerShape(0.dp)),
+    OutlinedTextField(
+      shape = RoundedCornerShape(5.dp),
+      modifier = Modifier.fillMaxWidth(),
       value = password, onValueChange = { password = it}, label = { Text("Password") },  visualTransformation = PasswordVisualTransformation())
   }
+    Spacer(modifier = Modifier.height(10.dp))
     Button(
       modifier = Modifier.fillMaxWidth(),
-      shape = RoundedCornerShape(0.dp),
+      shape = RoundedCornerShape(5.dp),
       onClick = {
       if(username.text == "admin" && password.text == "admin") nav.navigate("home")
-
+      else  isError = true
     }) {
       Text(
-        modifier = Modifier.fillMaxWidth(),
-        text = "Login")
+        modifier = Modifier
+          .fillMaxWidth()
+          .align(Alignment.CenterVertically),
+        textAlign = TextAlign.Center,
+        text = "Login",
+        )
     }
   }
 
