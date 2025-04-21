c15c45b0fecf1a0b6e4d3f7aaede0e686c29507a
Seungjin Lee <dltmdwls154@gmail.com>
2025-04-21 23:51:16 +0900

    Test

--- app/src/main/java/com/example/kotlintestapp/ui/layouts/SelectTime.kt
+++ app/src/main/java/com/example/kotlintestapp/ui/layouts/SelectTime.kt
@@ -1,20 +1,17 @@
 package com.example.kotlintestapp.ui.layouts
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
-import com.example.kotlintestapp.R
+import com.example.kotlintestapp.ui.components.inputs.DropDown
 import com.example.kotlintestapp.ui.theme.*
 import com.example.kotlintestapp.ui.theme.TextSizes.h2
 import com.example.kotlintestapp.ui.theme.TextSizes.m2
@@ -27,98 +24,68 @@ fun SelectTime() {
 
   var show by remember { mutableStateOf(false) }
   Column(
-    modifier = Modifier.fillMaxSize(),
+    modifier = Modifier.fillMaxSize().padding(vertical = 32.dp),
     horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center
+    verticalArrangement = Arrangement.SpaceBetween
   ) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-      Text("예약시간", style = h2, color = N90)
-      Text("예약시간", style = m2, color = N70)
-    }
+
     Column(
-      modifier = Modifier.width(228.dp)
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.Center
     ) {
+      Spacer(modifier = Modifier.height(120.dp))
+      Column(
+        modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+      ) {
+        Text("예약시간", style = h2, color = N90)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text("원하시는 입장 시간을 선택해 주세요.", style = m2, color = N70)
+        Spacer(modifier = Modifier.height(76.dp))
+      }
+
+
+
 
       Row(
-        modifier = Modifier.fillMaxWidth().height(60.dp)
-          .border(width = 1.dp, color = if (show) Orange else N20, shape = RoundedCornerShape(8.dp))
-          .padding(horizontal = 16.dp)
-          .clickable(
-            enabled = !show
-          ) { show = !show },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
       ) {
-        Text("10:00", style = n2, color = N50)
-        Image(
-          painter = painterResource(R.drawable.dropdown), contentDescription = "dropdown",
-          modifier = Modifier.width(24.dp).height(24.dp)
+        DropDown(
+          onClose = { show = false },
+          modifier = Modifier.weight(1f).height(60.dp),
+          items = listOf("123v", "123v", "123v", "123v", "123v", "123v", "123v")
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        DropDown(
+          onClose = { show = false },
+          modifier = Modifier.weight(1f).height(60.dp),
+          items = listOf("123v", "123v", "123v", "123v", "123v", "123v", "123v")
         )
       }
-
-      val hourScrollState = rememberScrollState()
-
-      DropdownMenu(
-        modifier = Modifier.background(White)
-          .clip(shape = RoundedCornerShape(8.dp))
-          .width(228.dp)
-          .heightIn(max = 270.dp).padding(0.dp),
-        expanded = show,
-        onDismissRequest = { show = false },
-        scrollState = hourScrollState,
-        offset = DpOffset(0.dp, 0.dp),
-        properties = PopupProperties(
-          focusable = false,
-          dismissOnBackPress = true,
-          dismissOnClickOutside = true,
-          clippingEnabled = true
-        )
-      ) {
-        DropdownMenuItem(
-          modifier = Modifier.fillMaxWidth().height(50.dp).padding(horizontal = 10.dp),
-          text = { Text("Option 1") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 2") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 1") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 2") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 1") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 2") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 1") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 2") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 1") },
-          onClick = { /* Do something... */ }
-        )
-        DropdownMenuItem(
-          text = { Text("Option 2") },
-          onClick = { /* Do something... */ }
-        )
-      }
-
-
     }
+    Row(
+      modifier = Modifier.fillMaxWidth().height(56.dp).padding(horizontal = 24.dp),
+    ) {
+      TextButton(
+        onClick = { },
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.clip(RoundedCornerShape(8.dp)).fillMaxHeight().weight(1f),
+        border = BorderStroke(1.dp, N30)
+      ) {
+        Text(text = "이전", color = N70, style = n2)
+      }
+      Spacer(modifier = Modifier.width(8.dp))
+      TextButton(
+        onClick = { },
+        modifier = Modifier.clip(RoundedCornerShape(8.dp)).background(color = Orange).weight(1f).fillMaxHeight(),
+        shape = RoundedCornerShape(8.dp),
+
+        ) {
+        Text(text = "다음", color = White, style = n2)
+      }
+    }
+
   }
+
 }
